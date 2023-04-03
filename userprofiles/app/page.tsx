a13df123edb124cb0bb3f4b2a0a777c47aa34df0
hiraaziz <hiraaziz1998@gmail.com>
2023-04-04 02:32:01 +0500

    Update page.tsx

--- userprofiles/app/page.tsx
+++ userprofiles/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="h-screen w-full p-8">
+      <h1 className="font-bold text-3xl mb-10">
+        Nextjs 13 Contentful Application
+      </h1>
+      <Link
+        href="/post"
+        className="bg-purple-400 px-4 py-2 rounded-md font-medium text-md text-white"
+      >
+        Go to Post
+      </Link>
+    </div>
+  );
 }
